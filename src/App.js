6fd138eff9671809ed3b8075927c4fabff6e9b49
Desarrollo 2 <desarrollo2@MacBook-Pro-de-Desarrollo-2.local>
2020-05-13 20:07:21 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Bye there. Adasd s
+         Bye there. Adasd ss
         </p>
         <a
           className="App-link"
